a19b08b41842a0031d9d2d37b6d573be490f4ec8
JM4r10 <oiram1585@hotmail.com>
2023-02-16 00:07:05 -0500

    Consume Backend API

--- src/pages/Customers.js
+++ src/pages/Customers.js
@@ -1,3 +1,31 @@
-export default function Customers(){
-    return <h1>Hello customers</h1>
+import { useEffect, useState } from "react";
+import { v4 as uuid4 } from 'uuid';
+
+export default function Customers() {
+
+    const [customers, setCustomers] = useState();
+
+    useEffect(() => {
+        console.log('Fetching...')
+        fetch('http://localhost:8000/api/customers')
+            .then((response => response.json()))
+            .then((data) => {
+                console.log(data.customers);
+                setCustomers(data.customers);
+            })
+            .catch((e) => console.log(e))
+    }, []);
+    return (
+        <>
+            <h1>Here are our customers:</h1>
+            {customers ?
+                customers.map((customer) => {
+                    return <p
+                        key={uuid4()}
+                    >
+                        $ {customer.name} 
+                        </p>
+                })
+                : null}
+        </>)
 }
